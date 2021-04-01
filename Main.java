18a5f1acd361b80fb15106d600de629b7965ab03
oberlado <78648733+oberlado@users.noreply.github.com>
2021-04-01 19:03:26 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -59,11 +59,4 @@ class MyThread extends Thread {
     float[] getArr() {
         return arr;
     }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-    }
-}
 }
